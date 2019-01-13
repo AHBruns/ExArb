46bfe05ca9d5f23292bb298ff5a7585f0adc48d8
alex.bruns <alex.h.bruns@gmail.com>
2019-01-13 01:57:53 -0600

    AB :: Basic startup sequence

--- src/ExArb/Core.java
+++ src/ExArb/Core.java
@@ -1,22 +1,25 @@
 package ExArb;
 
 import ExArb.Networking.NetworkManager;
-import com.google.gson.stream.JsonReader;
+import ExArb.Networking.Parsers.GetCurrenciesCompanion;
+import ExArb.Networking.Parsers.GetMarketsCompanion;
+import ExArb.Pathing.PathingManager;
+import ExArb.Structures.State;
 
 public class Core {
 
-    public static void main(String[] args) {
-        try {
-            JsonReader r = new NetworkManager().ExecuteGetMarkets();
-            r.beginObject();
-            System.out.println("\n\n-----\n\n");
-            System.out.println(r.nextName());
-            r.skipValue();
-            System.out.println(r.nextName());
+    public static void main(String[] args) throws Exception {
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // setup main state & manager objects
+        State state = new State();
+        NetworkManager nm = new NetworkManager();
+        PathingManager pm = new PathingManager(state);
+
+        // setup state with basic info
+        GetCurrenciesCompanion.parse(nm.ExecuteGetCurrencies(), state);
+        GetMarketsCompanion.parse(nm.ExecuteGetMarkets(), state);
+
+
     }
 
 }
