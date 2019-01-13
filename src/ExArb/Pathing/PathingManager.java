2ae594054112a8dcff573760a211a747a63b42a6
alex.bruns <alex.h.bruns@gmail.com>
2019-01-13 01:57:02 -0600

    AB :: Pathing

---
+++ src/ExArb/Pathing/PathingManager.java
@@ -0,0 +1,12 @@
+package ExArb.Pathing;
+
+import ExArb.Structures.State;
+
+public class PathingManager {
+
+    private State state;
+
+    public PathingManager(State s) {
+        this.state = s;
+    }
+}
